e5a99220cf89d15c528d0d8c48fcb1cca1ee47f7
Geronimo <geronimo.alvarez454@pascualbravo.edu.co>
2024-09-08 15:18:18 -0500

    Upload Project

---
+++ ProyectoFestivos.Dominio/Entidades/Festivo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFestivos.Dominio.Entidades
+{
+    [Table("Festivo")]
+    public class Festivo
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Nombre"),StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Column("Dia")]
+        public int Dia { get; set; }
+
+        [Column("Mes")]
+        public int Mes { get; set; }
+
+        [Column("DiasPascua")]
+        public int DiasPascua { get; set; }
+
+        [Column("IdTipo")]
+        public int Id_Tipo { get; set; }
+    }
+}
---
+++ ProyectoFestivos.Dominio/Entidades/Tipo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFestivos.Dominio.Entidades
+{
+    [Table("Tipo")]
+    public class Tipo
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Tipo"), StringLength(100)]
+        public string Nombre { get; set; }
+    }
+}
