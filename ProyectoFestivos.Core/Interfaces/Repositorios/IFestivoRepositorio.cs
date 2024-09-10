ef443a5b9a002eaebbf628c560cca180af4a549e
Geronimo <geronimo.alvarez454@pascualbravo.edu.co>
2024-09-09 19:42:00 -0500

    Update Project

---
+++ ProyectoFestivos.Core/Interfaces/Repositorios/IFestivoRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using ProyectoFestivos.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festivos.Core.Interfaces.Repositorios
+{
+    public interface IFestivoRepositorio
+    {
+        Task<IEnumerable<Festivo>> ObtenerTodos();
+        Task<Festivo> ObtenerPorId(int id);
+        Task<IEnumerable<Festivo>> BuscarPorNombre(string nombre);
+        Task<Festivo> Agregar(Festivo festivo);
+        Task<Festivo> Modificar(Festivo festivo);
+        Task<bool> Eliminar(int id);
+    }
+}
+
---
+++ ProyectoFestivos.Core/Interfaces/Servicios/ITipoServicio.cs
@@ -0,0 +1,18 @@
+﻿using ProyectoFestivos.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festivos.Core.Interfaces.Servicio
+{
+    public interface ITipoServicio
+    {
+        Task<IEnumerable<Tipo>> ObtenerTodos();
+        Task<Tipo> ObtenerPorId(int id);
+        Task<Tipo> Agregar(Tipo tipo);
+        Task<Tipo> Modificar(Tipo tipo);
+        Task<bool> Eliminar(int id);
+    }
+}
